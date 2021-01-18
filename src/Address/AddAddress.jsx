d0df310d18492c22cafa555f4ca0a83a79a52a48
Rajvi Khatri <rak@narola.email>
2021-01-18 17:06:42 +0530

    Add new address page change

--- src/Address/AddAddress.jsx
+++ src/Address/AddAddress.jsx
@@ -1,32 +1,34 @@
 import React, { Component } from 'react'
-import SideNav from '../SideNav/SideNav'
 import moment from 'moment';
-import { Form, Input, Button, notification } from 'antd';
+import { Form, Input, notification } from 'antd';
 import { saveUserAddress } from '../Services/AddressAPI';
-
+import Loader from '../Loader/Loader';
 export default class AddAddress extends Component {
 
+  formRef = React.createRef();
+
   state = {
-    newAddress: []
+    loading: false
   }
 
-  changeHandler = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
+  componentDidMount() {
+    this.setState({ loading: true })
+    setTimeout(() => {
+      this.setState({ loading: false })
+    }, 1000);
   }
 
   render() {
 
-    const addAddress = () => {
+    const addAddress = values => {
       const data = {
         UserId: parseInt(localStorage.getItem('userID')),
-        Address1: this.state.Address1,
-        Address2: this.state.Address2,
-        Country: this.state.Country,
-        Province: this.state.Province,
-        City: this.state.City,
-        PostalCode: this.state.postalCode,
+        Address1: values.Address1,
+        Address2: values.Address2,
+        Country: values.Country,
+        Province: values.Province,
+        City: values.City,
+        PostalCode: values.postalCode,
         CreatedBy: parseInt(localStorage.getItem('userID')),
         ModifiedBy: parseInt(localStorage.getItem('userID')),
         DateCreated: moment(new Date()).format(),
@@ -34,18 +36,19 @@ export default class AddAddress extends Component {
       }
       saveUserAddress(data).then(res => {
         if (res.data.data === -1) {
-          notification.open({
+          notification.info({
             message: 'Error',
             description: 'A company with same data address exist already!'
           });
         } else if (res.data.data !== -1) {
-          notification.open({
+          this.formRef.current.resetFields();
+          notification.success({
             message: 'Success',
             description: 'Company data has been added successfully!'
           });
         }
       }).catch(err => {
-        notification.open({
+        notification.error({
           message: 'Error',
           description: 'There was an error while adding Company Data!'
         });
@@ -53,61 +56,59 @@ export default class AddAddress extends Component {
     }
 
     return (
-      <div>
-        <div>
-          <SideNav />
+
+      <>
+        {this.state.loading ? <Loader /> :
           <div className="index-main">
             <div className="edit-sec mt-80"><h2>Add Address</h2></div>
             <div className="addticketform ml-4">
               <div className="form-border p-4 w-30 mt-5 crd-wrap">
-                <Form className="card-body" onFinish={addAddress}>
+                <Form className="card-body" onFinish={addAddress} ref={this.formRef}>
                   <div className="form-group">
                     <label>Address Line 1</label>
-                    <Form.Item >
-                      <Input value={this.state.Address1} className="w-100 inputstyle" name="Address1" onChange={(e) => this.changeHandler(e)} />
+                    <Form.Item name="Address1" rules={[{ required: true, message: "Please enter your Address Line 1!" }]} >
+                      <Input className="w-100 inputstyle" />
                     </Form.Item>
                   </div>
                   <div className="form-group">
                     <label>Address Line 2</label>
-                    <Form.Item>
-                      <Input value={this.state.Address2} className="w-100 inputstyle" name="Address2" onChange={(e) => this.changeHandler(e)} />
+                    <Form.Item name="Address2" rules={[{ required: true, message: "Please enter your  Address Line 2!" }]}>
+                      <Input className="w-100 inputstyle" />
                     </Form.Item>
                   </div>
                   <div className="form-group">
                     <label>City</label>
-                    <Form.Item>
-                      <Input value={this.state.City} className="w-100 inputstyle" name="City" onChange={(e) => this.changeHandler(e)} />
+                    <Form.Item name="City" rules={[{ required: true, message: "Please enter your City!" }]}>
+                      <Input className="w-100 inputstyle" />
                     </Form.Item>
                   </div>
                   <div className="form-group">
                     <label>Province</label>
-                    <Form.Item>
-                      <Input value={this.state.Province} className="w-100 inputstyle" name="Province" onChange={(e) => this.changeHandler(e)} />
+                    <Form.Item name="Province" rules={[{ required: true, message: "Please enter your Province!" }]}>
+                      <Input className="w-100 inputstyle" />
                     </Form.Item>
                   </div>
                   <div className="form-group">
                     <label>Country</label>
-                    <Form.Item>
-                      <Input value={this.state.Country} className="w-100 inputstyle" name="Country" onChange={(e) => this.changeHandler(e)} />
+                    <Form.Item name="Country" rules={[{ required: true, message: "Please enter your Country!" }]}>
+                      <Input className="w-100 inputstyle" />
                     </Form.Item>
                   </div>
                   <div className="form-group">
                     <label>Postal Code</label>
-                    <Form.Item>
-                      <Input value={this.state.postalCode} className="w-100 inputstyle" name="postalCode" onChange={(e) => this.changeHandler(e)} />
+                    <Form.Item name="postalCode" rules={[{ required: true, message: "Please enter your Postal Code!" }]}>
+                      <Input className="w-100 inputstyle" />
                     </Form.Item>
                   </div>
                   <div className="mt-4">
-                    <Form.Item>
-                      <Button type="primary" htmlType="submit" className="btn btn-orange-search">Add Address</Button>
-                    </Form.Item>
+                    <button type="submit" className="btn btn-blue btnManufacturer">Add Address</button>
                   </div>
                 </Form>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        }
+      </>
     )
   }
 }
