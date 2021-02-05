577d75a743894df2f42851064b21a76edfec2f54
Rajvi Khatri <rak@narola.email>
2021-02-05 16:38:34 +0530

    Removed then catch from service files

--- src/Services/Project.js
+++ src/Services/Project.js
@@ -1,51 +1,27 @@
 import axios from "axios";
 
 export function getProjectManufacturers(id) {
-    return axios.get(process.env.REACT_APP_API_URL + `api/projects/GetProjectManufacturers/${id}`).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.get(process.env.REACT_APP_API_URL + `api/projects/GetProjectManufacturers/${id}`);
 }
 
 export function getProjectCompany(id) {
-    return axios.get(process.env.REACT_APP_API_URL + `api/projects/GetProjectCompanies/${id}`).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.get(process.env.REACT_APP_API_URL + `api/projects/GetProjectCompanies/${id}`);
 }
 
 export function saveManufacturers(data) {
-    return axios.post(process.env.REACT_APP_API_URL + `api/misc/SaveManufacturer`, data).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.post(process.env.REACT_APP_API_URL + `api/misc/SaveManufacturer`, data);
 }
 
 export function saveCompany(data) {
-    return axios.post(process.env.REACT_APP_API_URL + `api/companies/SaveCompany`, data).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.post(process.env.REACT_APP_API_URL + `api/companies/SaveCompany`, data);
 }
 
 export function saveNewManufacturerInProject(data) {
-    return axios.post(process.env.REACT_APP_API_URL + `api/projects/SaveProjectManufacturer`, data).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.post(process.env.REACT_APP_API_URL + `api/projects/SaveProjectManufacturer`, data);
 }
 
 export function saveNewCompanyInProject(data) {
-    return axios.post(process.env.REACT_APP_API_URL + `api/projects/SaveProjectCompany`, data).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.post(process.env.REACT_APP_API_URL + `api/projects/SaveProjectCompany`, data);
 }
 
 export function saveProjectPicture(data) {
@@ -54,43 +30,32 @@ export function saveProjectPicture(data) {
             "Content-Type": "multipart/form-data",
             "Accept": "application/json"
         }
-    }).then(response => {
-        return response
-    }).catch(error => {
-        return error
     });
 }
 
 export function getSearchProjectsBy(data) {
-    return axios.post(process.env.REACT_APP_API_URL + `api/projects/GetSearchProjectsBy`, data).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.post(process.env.REACT_APP_API_URL + `api/projects/GetSearchProjectsBy`, data);
 }
 
 export function postSaveUserWorkHistory(data) {
-    return axios.post(process.env.REACT_APP_API_URL + `api/users/SaveUserWorkHistory`, data).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.post(process.env.REACT_APP_API_URL + `api/users/SaveUserWorkHistory`, data);
 }
 
 export function searchProjectsBy(city, projectName, buildingTypeId, contractorName) {
     var uri = `${process.env.REACT_APP_API_URL}api/projects/GetSearchProjectsBy/{CityProv:'${city}',ProjectName:'${projectName}',BuildingTypeId:'${buildingTypeId}',ContractorName:'${contractorName}'}`;
     var res = encodeURI(uri);
-    return axios.get(res).then(response => {
-        return response
-    }).catch(error => {
-        return error
-    });
+    return axios.get(res);
 }
 
 export function getGoogleAPIKey() {
-    return axios.get(process.env.REACT_APP_API_URL + `api/misc/GetGoogleSearchCode`).then(response => {
-        return response
-    }).catch(error => {
-        return error
+    return axios.get(process.env.REACT_APP_API_URL + `api/misc/GetGoogleSearchCode`);
+}
+
+export function addGlobalProject(data) {
+    return axios.post(process.env.REACT_APP_API_URL + `api/projects/SaveProject`, data, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+            "Accept": "application/json"
+        }
     });
 }
