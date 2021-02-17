21d143cc1ada862159fdb82c5b62a25795371ea6
Rajvi Khatri <rak@narola.email>
2021-02-17 12:12:27 +0530

    New API for Review 8

--- src/Services/Experience.js
+++ src/Services/Experience.js
@@ -15,3 +15,15 @@ export function deleteAnExperience(experienceId) {
 export function editAnExperience(userid, experienceId) {
     return axios.get(process.env.REACT_APP_API_URL + `api/users/GetUserCompanyHistoryItem/${userid}/${experienceId}`);
 }
+
+export function saveGlobalTrade(data) {
+    return axios.post(process.env.REACT_APP_API_URL + `api/misc/savetrade`, data);
+}
+
+export function saveGlobalRole(data) {
+    return axios.post(process.env.REACT_APP_API_URL + `api/misc/saveRole`, data);
+}
+
+export function saveGlobalBuildingType(data) {
+    return axios.post(process.env.REACT_APP_API_URL + `api/projects/saveBuildingType`, data);
+}
